9355359a16a660a0b4ddbde56c8c029570dd8143
anqi Sima <anqisima@anqideAir.attlocal.net>
2019-10-16 23:38:49 -0500

    add do response and too long too short response

--- rightversionhomework.R
+++ rightversionhomework.R
@@ -1,4 +1,13 @@
 
+#print the computer's response to user
+do_response <- function(bulls, attempts) {
+  if(bulls == 4) {
+    print(paste("You won in",attempts,"attempt(s)!"))
+  } else {
+    print(paste("You lost in", attempts, "attempt(s)!"))
+  }
+}
+
 #number of cows
 number_cows <- function(user_choice, computer_choice, bulls) {
   cows = sum(user_choice %in% computer_choice) - bulls
@@ -24,20 +33,11 @@ generate_computer_vector <- function() {
   return(computer_choice)
 }
 
-#print the computer's response to user
-do_response <- function(bulls, attempts) {
-  if(bulls == 4) {
-    print(paste("You won in",attempts,"attempt(s)!"))
-  } else {
-    print(paste("You lost in", attempts, "attempt(s)!"))
-  }
-}
-
 #prompt the user to input a guess and tell the user how many guesses they remain
 get_guess <- function(bulls, cows, attempts, total_attempts, wrong_attempts, computer_choice) {
   print(paste("Now you should input 4 digits for bulls & cows guess game, you just have", total_attempts,"chances!"))
   while (bulls != 4) {
-    number_string = readline("please enter the four numbers >")
+    number_string = readline("please enter the four numbers with no duplicate digits or 0s: >")
     attempts <- attempts + 1
     if(nchar(number_string) == 4) {
       user_choice = as.integer(unlist(strsplit(number_string,"")[[1]]))
@@ -48,24 +48,40 @@ get_guess <- function(bulls, cows, attempts, total_attempts, wrong_attempts, com
       if(bulls == 4) {
         break
       }
-    }
-      if(attempts == total_attempts) {
+    } else {
+      error = 0
+      while(error<3){
+        user_choice <- get_guess(computer_choice, total_attempts, attempts)
+        if(length(user_choice)>4){
+          print("You have input too long!")
+        }else if(length(user_choice)<4){
+          print("You have input too short!")
+        }else break
+        error = error + 1
+        attempts = attempts + 1
+      }
+      if(error==3){
+        print("You have consecutively input 3 times incorrect number, game over!")
         break
       }
-      print(paste("You left",(total_attempts - attempts),"chance(s)!"))
     }
-    do_response(bulls, attempts)
- }
-  
-  #guess the bulls and cows
-  bulls_and_cows <- function() {
-    bulls = 0
-    cows = 0
-    attempts = 0
-    total_attempts = 10
-    computer_choice = generate_computer_vector()
-    wrong_attempts = 0
-    get_guess(bulls, cows, attempts, total_attempts, wrong_attempts, computer_choice)
+    if(attempts == total_attempts) {
+      break
+    }
+    print(paste("You left",(total_attempts - attempts),"chance(s)!"))
   }
-  
-  bulls_and_cows()12
+  do_response(bulls, attempts)
+}
+
+#guess the bulls and cows
+bulls_and_cows <- function() {
+  bulls = 0
+  cows = 0
+  attempts = 0
+  total_attempts = 10
+  computer_choice = generate_computer_vector()
+  error = 0
+  get_guess(bulls, cows, attempts, total_attempts, error, computer_choice)
+}
+
+bulls_and_cows()
